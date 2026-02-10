56a710b54e2e546499a8d4d79465bb11e1852f47
Nathaniel Henry <nat.henry7@gmail.com>
2026-02-09 21:07:11 -0800

    Add OSM-specific submodule.

---
+++ src/openpois/osm/__init__.py
@@ -0,0 +1,7 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Henry Spatial Analysis. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""Open POIs"""
+
+__version__ = "0.0.1"
