3d58862eee2f7a3de74a35a70079f11c45b021c5
Nathaniel Henry <nat.henry7@gmail.com>
2026-02-10 00:02:18 -0800

    Drop unnecessary metadata in init file.

--- src/openpois/osm/__init__.py
+++ src/openpois/osm/__init__.py
@@ -1,7 +1,1 @@
-#   -------------------------------------------------------------
-#   Copyright (c) Henry Spatial Analysis. All rights reserved.
-#   Licensed under the MIT License. See LICENSE in project root for information.
-#   -------------------------------------------------------------
-"""Open POIs"""
 
-__version__ = "0.0.1"
