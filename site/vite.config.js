ba6075abee5fe236c066bbafb6e75985954cd39d
Nathaniel Henry <nat.henry7@gmail.com>
2026-03-17 15:06:11 -0700

    Roll up into smaller chunks.

--- site/vite.config.js
+++ site/vite.config.js
@@ -4,4 +4,18 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/',
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/ol-mapbox-style')) return 'ol-mapbox-style'
+          if (id.includes('node_modules/ol-pmtiles')) return 'ol-pmtiles'
+          if (id.includes('node_modules/ol/')) return 'ol'
+          if (id.includes('node_modules/apache-arrow')) return 'arrow'
+          if (id.includes('node_modules/@duckdb')) return 'duckdb'
+          if (id.includes('node_modules/vue')) return 'vue'
+        },
+      },
+    },
+  },
 })
